bf9262d8905bccc02276a8c16c023083aaea5f47
ibaijimenez11 <ibai.jimmar@educa.jcyl.es>
2025-12-19 12:55:00 +0100

    actividad5

--- actividad5.js
+++ actividad5.js
@@ -1,1 +1,9 @@
-alert("Hola Mundo")
+function HolaMundo() {
+    alert("Hola Mundo");
+}
+const btn1=document.querySelector("#boton1");
+btn1.addEventListener("click",function(){
+    alert("Hola Mundo")
+});
+const btn2=document.querySelector("#boton2");
+btn2.addEventListener
